0d506c11ec883ee531266150b2cf3f9038748e29
Mallory LeeWong <m.c.leewong@gmail.com>
2020-04-10 19:43:01 -0400

    refactor: change getData to async await

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -1,12 +1,19 @@
+// create custom hook to fetch data
+
 import { useState, useEffect } from 'react';
 
 const useFetch = url => {
   const [data, setData] = useState([]);
 
+  // to use async await, create a helper async func to invoke inside useEffect
+  const getData = () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setData(data)
+  }
+
   useEffect(() => {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => setData(data));
+    getData()
   }, []);
 
   // important empty dependencies array passed in to avoid effect getting called after every render
