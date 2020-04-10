bbd84835ad24900c0f83f697cf092b4b7a86ca70
Mallory LeeWong <m.c.leewong@gmail.com>
2020-04-10 19:46:52 -0400

    fix: add async to getData function

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -6,14 +6,14 @@ const useFetch = url => {
   const [data, setData] = useState([]);
 
   // to use async await, create a helper async func to invoke inside useEffect
-  const getData = () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    setData(data)
-  }
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+  };
 
   useEffect(() => {
-    getData()
+    getData();
   }, []);
 
   // important empty dependencies array passed in to avoid effect getting called after every render
